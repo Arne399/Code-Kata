224fa6a08a2ef1e870e6b02e5aabc632b573924d
Raf Engelen <91262594+rafengelen@users.noreply.github.com>
2026-04-27 12:12:19 +0000

    geen leuke opdracht

--- Code Kata/Services/WorkScheduleService.cs
+++ Code Kata/Services/WorkScheduleService.cs
@@ -4,39 +4,78 @@ using Code_Kata.Entities.WorkSchedules;
 
 namespace Code_Kata.Services;
 
-public class WorkScheduleService
-{
-    public WorkScheduleEntry TrySchedule(List<Engineer> engineers, Incident incident)
-    {
-        var engineer = engineers.FirstOrDefault();
+using Code_Kata.Entities.Engineers;
+using Code_Kata.Entities.Incidents;
+using Code_Kata.Entities.WorkSchedules;
 
-        var lastWorkSchedule = State.WorkScheduleEntries.Where(ws => ws.EngineerId == engineer.Id)
-            .OrderBy(ws => ws.StartAt).LastOrDefault();
-        
-        var startAt = GetStartTime(lastWorkSchedule, incident, engineer);
-        var endAt = GetEndTime(startAt, incident);
+public class WorkScheduleService {
 
-        var newEntry = new WorkScheduleEntry
-        {
-            EngineerId = engineer.Id,
-            IncidentId = incident.Id,
-            StartAt = startAt,
-            EndAt = endAt
-        };
+	public WorkScheduleEntry OptimizeSchedule(List<Engineer> engineers, Incident incident){
+		var current = TimeOnly.FromDateTime(DateTime.Now);
+		var possible = new List<(Engineer eng, TimeOnly newStart, List<WorkScheduleEntry> newSchedule)>();
 
-        State.WorkScheduleEntries.Add(newEntry);
-        return newEntry;
-    }
+		foreach (var eng in engineers) {
+			if (!eng.Skills.Contains(incident.SkillType)) continue;
 
-    private TimeOnly GetStartTime(WorkScheduleEntry? lastWorkSchedule, Incident incident, Engineer engineer)
-    {
-        if (lastWorkSchedule is not null) return lastWorkSchedule.EndAt;
-       
-        return incident.ReportedAt > engineer.AvailableFrom ? incident.ReportedAt : engineer.AvailableFrom;
-    }
-    
-    private TimeOnly GetEndTime(TimeOnly startAt, Incident incident)
-    {
-        return startAt.AddMinutes(incident.EstimatedMinutes);
-    }
+			var allEntries = State.WorkSchedules.Where(ws => ws.EngineerId == eng.Id).ToList();
+			var fixedEntries = allEntries.Where(e => e.EndAt <= current).OrderBy(e => e.StartAt).ToList();
+			var pendingEntries = allEntries.Where(e => e.EndAt > current).ToList();
+			var pendingIncidents = State.Incidents.Where(i => pendingEntries.Select(e => e.IncidentId).Contains(i.Id)).ToList();
+			pendingIncidents.Add(incident);
+
+			var sortedPending = pendingIncidents.OrderByDescending(i => i.Severity * i.Impact).ThenBy(i => i.ReportedAt).ThenBy(i => i.Id).ToList();
+
+			var startTime = eng.AvailableFrom;
+			if (fixedEntries.Any()) {
+				startTime = TimeOnly.Max(startTime, fixedEntries.Last().EndAt);
+			}
+
+			var newPendingEntries = new List<WorkScheduleEntry>();
+			TimeOnly? newIncStart = null;
+			bool canFit = true;
+
+			foreach (var inc in sortedPending) {
+				var start = TimeOnly.Max(startTime, inc.ReportedAt);
+				var end = start.AddMinutes(inc.EstimatedMinutes);
+
+				if (end > eng.AvailableUntil || end > inc.Deadline) {
+					canFit = false;
+					break;
+				}
+
+				var entry = new WorkScheduleEntry {
+					EngineerId = eng.Id,
+					IncidentId = inc.Id,
+					StartAt = start,
+					EndAt = end
+				};
+
+				newPendingEntries.Add(entry);
+
+				if (inc == incident) newIncStart = start;
+
+				startTime = end;
+			}
+
+			if (!canFit) continue;
+
+			var totalPendingMinutes = newPendingEntries.Sum(e => e.Minutes);
+			var totalFixedMinutes = fixedEntries.Sum(e => e.Minutes);
+			if (totalPendingMinutes + totalFixedMinutes > eng.MaxWorkMinutes) continue;
+
+			var newSchedule = fixedEntries.Concat(newPendingEntries).ToList();
+			possible.Add((eng, newIncStart.Value, newSchedule));
+		}
+
+		if (!possible.Any()) return null;
+
+		var best = possible.OrderBy(p => p.newStart).First();
+
+		
+		State.WorkSchedules.RemoveAll(ws => ws.EngineerId == best.eng.Id);
+		State.WorkSchedules.AddRange(best.newSchedule);
+		incident.AssignedEngineerId = best.eng.Id;
+
+		return best.newSchedule.First(ws => ws.IncidentId == incident.Id);
+	}
 }

cf6d0eb0247e49964443183f35fc7078835ab938
Raf Engelen <91262594+rafengelen@users.noreply.github.com>
2026-04-27 11:26:37 +0000

    WorkScheduleService

--- Code Kata/Services/WorkScheduleService.cs
+++ Code Kata/Services/WorkScheduleService.cs
@@ -1,6 +1,19 @@
 namespace Code_Kata.Services;
 
-public class WorkScheduleService
-{
-    
+public class WorkScheduleService {
+
+	public WorkScheduleEntry OptimizeSchedule(Engineer engineer, Incident incident){
+		var lastWorkSchedule = State.WorkSchedules.Where(ws => ws.EngineerId == engineer.Id).OrderBy(ws => ws.StartAt).LastOrDefault();
+
+		var newEnty = new WorkScheduleEntry
+		{	
+			EngineerId = engineer.Id,
+			IncidentId = Incident.Id,
+			StartAt = lastWorkSchedule.EndAt,
+			EndAt = lastWorkSchedule.EndAt.AddMinutes(incident.EstimatedMinutes)
+		};
+
+		State.WorkSchedules.add(newEntry);
+		return newEntry;
+	}
 }
